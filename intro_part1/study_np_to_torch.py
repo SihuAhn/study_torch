bd7ae8c0aa07d0f932b152f91beb4ea992b36ed4
sihuahn <sihu.ahn0605@gmail.com>
2021-04-16 16:55:09 +0900

    practice np to torch and back

---
+++ intro_part1/study_np_to_torch.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+
+np.set_printoptions(precision=8)
+a = np.random.rand(4,3)
+print(a)
+
+b = torch.from_numpy(a)
+print(b)
+print(b.numpy())
+
+
+print(b.mul(2))
+print(a)
