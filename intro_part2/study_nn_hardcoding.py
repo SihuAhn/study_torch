b53ab150b31ff68c95c09cf3a35e0a4e4927be06
sihuahn <sihu.ahn0605@gmail.com>
2021-04-18 22:30:52 +0900

    network architecture

---
+++ intro_part2/study_nn_hardcoding.py
@@ -0,0 +1,31 @@
+from data_call import *
+
+
+
+def activation(x):
+    return 1/(1+torch.exp(-x))
+
+def multi_Layer_NW(inputUnits, hiddenUnits, outputUnits):
+
+    n_input = inputUnits
+    n_hidden = hiddenUnits
+    n_output = outputUnits
+
+    W1 = torch.randn(n_input, n_hidden)
+    W2 = torch.randn(n_hidden, n_output)
+
+    B1 = torch.randn(1, n_hidden)
+    B2 = torch.randn(1, n_output)
+
+    return W1, W2, B1, B2
+
+def calc_output(features, W1, W2, B1, B2):
+    h = activation(torch.mm(features, W1) + B1)
+    output = activation(torch.mm(h, W2) + B2)
+    return output
+
+def softmax(x):
+    return torch.exp(x) / torch.sum(torch.exp(x), dim=1).view(-1, 1)
+
+
+
