37fd9f9163d64c9535a32b4948b7d39a19e26078
sihuahn <sihu.ahn0605@gmail.com>
2021-04-18 22:32:30 +0900

    testing file

---
+++ intro_part2/main.py
@@ -0,0 +1,35 @@
+# import helper
+from intro_part2.study_nn import Network_basic
+from intro_part2.study_nn_F import Network
+from study_nn_hardcoding import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+features = images.reshape(64, 784)
+W1, W2, B1, B2 = multi_Layer_NW(features.shape[1], 256, 10)
+
+# probabilities = calc_output(features, W1, W2, B1, B2)
+# result = softmax(probabilities)
+# print(result.sum(dim=1))
+# print(probabilities.shape)
+# print(result.max(dim=1))
+
+# model = Network_basic()
+# print(model.forward(features).max(dim=1))
+
+model = Network()
+model.hidden1.weight.data.normal_(std=0.01)
+model.hidden1.bias.data.fill_(0)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+images.resize_(64, 1, 784)
+
+img_idx = 0
+ps = model.forward(images[img_idx, :])
+
+img = images[img_idx]
+plt.imshow(images[1].numpy().squeeze())
+plt.show()
+print(ps.shape)
