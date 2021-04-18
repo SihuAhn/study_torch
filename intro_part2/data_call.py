c6d9b488b6af49431305f7ec334ed71c525971d0
sihuahn <sihu.ahn0605@gmail.com>
2021-04-18 22:32:16 +0900

    call the MNIST dataset

---
+++ intro_part2/data_call.py
@@ -0,0 +1,22 @@
+from torchvision import datasets, transforms
+import torch
+
+torch.manual_seed(7)
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+
+trainset = datasets.MNIST('data/MNIST_data', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# loop syntax
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+
+# print(f'type of images = {type(images)}\n'
+#       f'shape of images = {images.shape}\n'
+#       f'shape of lables = {labels.shape}')
+
+# plt.imshow(images[0].numpy().squeeze(), cmap='Greys_r')
+# plt.show()
