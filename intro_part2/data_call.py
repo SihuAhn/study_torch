433bfa5c5cc3668cad4e607843af1b9d52f8fcb7
sihuahn <sihu.ahn0605@gmail.com>
2021-04-22 21:12:13 +0900

    modified path

--- intro_part2/data_call.py
+++ intro_part2/data_call.py
@@ -6,7 +6,7 @@ transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,)),
                                 ])
 
-trainset = datasets.MNIST('data/MNIST_data', download=True, train=True, transform=transform)
+trainset = datasets.MNIST('D:/code/study_torch/data/MNIST_data', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 # loop syntax
