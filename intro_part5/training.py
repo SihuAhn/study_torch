47c7523fcc14c37cec9fe58398a7feef51f0001b
sihuahn <sihu.ahn0605@gmail.com>
2021-04-28 20:26:21 +0900

    updated to get losses

--- intro_part5/training.py
+++ intro_part5/training.py
@@ -5,7 +5,7 @@ model = Classifier()
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0003)
 
-epochs = 5
+epochs = 30
 steps = 0
 
 train_losses, test_losses = [], []
@@ -21,12 +21,34 @@ for e in range(epochs):
 
         running_loss += loss.item()
     else:
-        x, y = next(iter(testloader))
-        ps = torch.exp(model(x))
-        top_p, top_class = ps.topk(1, dim=1)
-        equals = top_class == y.view(*top_class.shape)
+        test_loss = 0
+        accuracy = 0
 
-        accuracy = torch.mean(equals.type(torch.FloatTensor))
-        print(f'Accuracy: {accuracy.item()*100}%')
+        with torch.no_grad() as no:
+            model.eval()
+            for images, labels in testloader:
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(model(images))
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+        model.train()
+
+        train_losses.append(running_loss/len(trainloader))
+        test_losses.append(test_loss/len(testloader))
+
+        print(f'Epoch: {e+1}/{epochs}..\n'
+              f'Training Loss: {train_losses[-1]:.3f}..\n'
+              f'Test Loss: {test_losses[-1]:.3f}..\n'
+              f'Test Accuracy: {accuracy/len(testloader):.3f}..')
+
+# import matplotlib.pyplot as plt
+# plt.plot(train_losses, label='Training loss')
+# plt.plot(test_losses, label='Validation loss')
+# plt.legend(frameon=False)
+# plt.show()
 
 torch.save(model.state_dict(), 'checkpoint.pth')
