8b06e42232d8e4efa283c5fe072881fa9d138b02
sihuahn <sihu.ahn0605@gmail.com>
2021-04-29 17:00:27 +0900

    called real data and data augmentation

--- intro_part7/data_loader.py
+++ intro_part7/data_loader.py
@@ -5,22 +5,32 @@ from torchvision import datasets, transforms
 
 import helper
 
-transform = transforms.Compose([transforms.Resize(256),
-                                transforms.CenterCrop(224),
-                                transforms.ToTensor()])
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor()])
 
-train_set = datasets.ImageFolder('D:/code/study_torch/data/Cat_Dog_data/train/', transform=transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor()])
 
-test_set = datasets.ImageFolder('D:/code/study_torch/data/Cat_Dog_data/test/', transform=transform)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True)
+torch.manual_seed(777)
+train_set = datasets.ImageFolder('D:/code/study_torch/data/Cat_Dog_data/train/', transform=train_transforms)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
 
-data_iter = iter(test_loader)
+test_set = datasets.ImageFolder('D:/code/study_torch/data/Cat_Dog_data/test/', transform=test_transforms)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
 
-images, labels = next(data_iter)
-fig, axes = plt.subplots(figsize=(10,4), ncols=4)
-for ii in range(4):
-    ax = axes[ii]
-    helper.imshow(images[ii], ax=ax, normalize=False)
+def temp(loader):
+    data_iter = iter(loader)
+
+    images, labels = next(data_iter)
+    fig, axes = plt.subplots(figsize=(10,4), ncols=4)
+    for ii in range(4):
+        ax = axes[ii]
+        helper.imshow(images[ii], ax=ax, normalize=False)
+
+temp(train_loader)
+temp(test_loader)
 
 plt.show()
